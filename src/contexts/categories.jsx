9884a36f3500ed41b240fc5ca857f4c5a34507ed
Darren Eymundson <darren.eymundson@computronix.com>
2025-02-16 14:20:53 -0700

    Begin using apollo GraphQL

--- src/contexts/categories.jsx
+++ src/contexts/categories.jsx
@@ -1,12 +1,31 @@
 import { createContext, useEffect, useState } from "react";
-import { getCategoriesAndDocuments } from "../backend/firebase";
+import { gql, useQuery } from "@apollo/client";
+// import { getCategoriesAndDocuments } from "../backend/firebase";
 
 export const CategoriesContext = createContext({
   categories: {},
 });
 
+const COLLECTIONS = gql`
+  query GetCollections {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categories, setCategories] = useState({});
+
+  console.log(data);
 
   useEffect(() => {
     (async () => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,43 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
 import App from "./App.jsx";
 import { CartProvider, CategoriesProvider, UserProvider } from "./contexts";
 
+import "./index.css";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://crwn-clothing.com/",
+    fetchOptions: {
+      // mode: "no-cors",
+      // referrerPolicy: "unsafe-url",
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  }),
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>
 );
